ae956b1af95e879cc6a54b0c1feb52cdcae68909
Lawrence L. O'Boyle II <oboylel@mail.gvsu.edu>
2015-03-19 10:21:29 -0400

    New lab, and finished for the old lab

--- lab7/p19.s
+++ lab7/p19.s
@@ -2,7 +2,7 @@
 ; The base address of array b is in r2
 ; The base address of array c is in r3
 
-addi r13, r0, 12
+addi r13, r0, 8
 LOOP: lw r4, 0(r1)		; Load from a(0) into r4
       lw r5, 0(r2)		; Load from b(0) into r5
       lw r6, 4(r1)		; Load from a(4) into r6
@@ -12,7 +12,7 @@ LOOP: lw r4, 0(r1)		; Load from a(0) into r4
       lw r11, 12(r1)	; Load from a(12) into r11
       lw r12, 12(r2)	; Load from b(12) into r12
 
-      subi r13, r13, 4   ; Decrement the counter
+      subi r13, r13, 4  ; Decrement the counter
       
       add r7, r4, r5	; r7 = r4 + r5
       sw  0(r3), r7		; store result back in array c
@@ -31,7 +31,7 @@ LOOP: lw r4, 0(r1)		; Load from a(0) into r4
       bnez r13, LOOP	; Branches if it isn't 0
       addi r3, r3, 16	; Shifts the array
 
-sw 0(r3), r0 		; set c[0] to 0. (not important, just something to do after the loop.)
+sw 0(r3), r0 		    ; set c[0] to 0. (not important, just something to do after the loop.)
 
 nop;
 nop; 
