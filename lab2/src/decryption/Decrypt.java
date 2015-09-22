8a4c7a2f3aa601a30dab45a92b73cf41270545ae
Lawrence L. O'Boyle <oboylelii@gmail.com>
2015-09-22 15:50:52 -0400

    Initial commit

---
+++ lab2/src/decryption/Decrypt.java
@@ -0,0 +1,8 @@
+package decryption;
+
+/**
+ * Created by lawrence on 9/22/15.
+ */
+public class Decrypt
+{
+}
---
+++ lab2/src/encryption/Encrypt.java
@@ -0,0 +1,8 @@
+package encryption;
+
+/**
+ * Created by lawrence on 9/22/15.
+ */
+public class Encrypt
+{
+}
