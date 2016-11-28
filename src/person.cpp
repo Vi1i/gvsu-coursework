aa76507ee713bbf90e271dff027f504756dcc3ab
Lawrence O'Boyle II <lawrence@oboylelii.com>
2016-11-28 16:37:42 -0500

    Initial commit

--- src/person.cpp
+++ src/person.cpp
@@ -2,3 +2,13 @@
 // Created by Vi1i on 11/22/16.
 //
 
+#include "../headers/person.h"
+
+Person::Person(unsigned int id, Tuberculosis tb, School school) {
+    this->_id = id;
+    this->_tb = tb;
+    this->_school = school;
+}
+Person::~Person() {
+
+}
