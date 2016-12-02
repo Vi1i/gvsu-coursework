a00d6723a46c41f89b36426fae56d6dcbdb3bbd2
Vi1i <vi1i.github@gmail.com>
2016-12-02 13:39:55 -0500

    Just a quick test for the DB

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "headers/DBConnetionManager.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    DBConnectionManager connectionManager = DBConnectionManager(
+            "EntityMaster1314", "localhost", "root", "Voidtak3?");
+
+    if(connectionManager.ConnectionCheck()) {
+        connectionManager.QueryCheck("SELECT * FROM baseEntityMaster1314");
+        connectionManager.Query();
+        std::cerr << "Passed!" << std::endl;
+        std::string * row = connectionManager.GetRow();
+
+        for(auto it = row->begin(); it != row->end(); ++it) {
+            std::cout << *it << std::endl;
+        }
+    }else{
+        std::cerr << "Failed!" << std::endl;
+    }
     return 0;
 }
