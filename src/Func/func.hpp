05051a6d672470aba1b86dff1b099c393a647f55
Lawrence O'Boyle II <lawrence@oboylelii.com>
2017-02-16 12:00:30 -0500

    Final push

--- src/Func/func.hpp
+++ src/Func/func.hpp
@@ -3,9 +3,207 @@
 
 #include <sys/stat.h>
 namespace nbdc {
+std::string total_cat("F1vs8KymEXNmmIod");
+
 inline bool FileExists(const std::string & filename) {
 	struct stat buffer;   
 	return (stat (filename.c_str(), &buffer) == 0); 
+};
+
+template<typename T>
+inline void PrintElement(T t, std::ostringstream& os, const int& width) {
+	const char separator = ' ';
+	os << std::left << std::setw(width) << std::setfill(separator) << t;
+}
+
+void ReadFile(const std::string& filename,
+			  	std::map<std::string, std::vector<std::vector<std::string>>>& data,
+			  	std::map<std::string, std::map<std::string, int>>& data_info) {
+	//Opening the file
+	std::ifstream in_file(filename);
+	//Where the lines from the file will be stored
+	std::string line;
+
+	//While loop to get the line from the file
+	while(std::getline(in_file, line)) {
+		//A line from the file
+		std::istringstream in_line(line);
+		//Where the word in the line will be stored
+		std::string token;
+		//Temp vector variable
+		std::vector<std::string> row;
+		//Counter varaible for pos
+		int count(0);
+		//File seperator
+		char sep = ' ';
+		//Category string
+		std::string cat("");
+
+		//Loop to get each "word" out of the line
+		while(std::getline(in_line, token, sep)) {
+			//The first "word" is the category, that I can't use and is only for
+			//training sets
+			if(count == 0) {
+				cat += token;
+				count++;
+				continue;
+			}
+
+			//Adds the token to the local stream
+			row.push_back(token);
+		}
+		data_info[cat]["Words"] += row.size();
+		//Add the string vector to the category
+		data[cat].push_back(row);
+	}
+	int temp_docs(0);
+	int temp_words(0);
+	for(auto const& map : data) {
+		temp_docs += map.second.size();
+		data_info[map.first]["Docs"] = map.second.size();
+
+		temp_words += map.second.size();
+	}
+
+	data_info[total_cat]["Docs"] = temp_docs;
+	data_info[total_cat]["Words"] = temp_words;
+};
+
+void ProcessData(const std::map<std::string, std::vector<std::vector<std::string>>>& data,
+				 const std::map<std::string, std::map<std::string, int>> & data_info,
+				 std::map<std::string, std::map<std::string, double>>& processed_data,
+				 std::map<std::string, std::map<std::string, double>>& processed_data_info,
+				 std::map<std::string, std::map<std::string, int>>& cat_vocab,
+				 std::map<std::string, int>& vocab) {
+
+	//Gets the indivudal category vocab
+	for(auto const& map : data) {
+		std::map<std::string, int> c_v;
+		for(auto const& vector : map.second) {
+			for(auto const& word : vector) {
+				// std::cout << word << std::endl;
+				c_v[word]++;
+			}
+		}
+		cat_vocab[map.first] = c_v;
+	}
+
+	//Gets the total counts of all words found
+	for(auto const& map : cat_vocab) {
+		for(auto const& word_counts : map.second) {
+			vocab[word_counts.first] += word_counts.second;
+		}
+	}
+
+	int total_u_words = vocab.size();
+
+	//Calculates the probability of the word for each category
+	for(auto const& word : vocab) {
+		for(auto const& map : cat_vocab) {
+			if(!(map.second.find(word.first) == map.second.end())) {
+				double top = (double)map.second.at(word.first) + (double)1;
+				double bottom = (double)data_info.at(map.first).at("Words") + total_u_words;
+				processed_data[map.first][word.first] = top / bottom;
+			}else{
+				double top = 1;
+				double bottom = (double)data_info.at(map.first).at("Words") + total_u_words;
+				processed_data[map.first][word.first] = top / bottom;
+			}
+		}
+	}
+
+	int total_docs = data_info.at(nbdc::total_cat).at("Docs");
+	for(auto const& map : data_info) {
+		if(map.first == total_cat) {
+			continue;
+		}
+		std::map<std::string, double> p_info;
+		for(auto const& info : map.second) {
+			if(info.first == "Words") {
+				continue;
+			}
+			p_info[info.first] = (double)info.second / (double) total_docs;
+		}
+		processed_data_info[map.first] = p_info;
+	}
+};
+
+void AnalyzeData(const std::map<std::string, std::map<std::string, double>>& processed_data,
+				 const std::map<std::string, std::map<std::string, double>>& processed_data_info,
+				 const std::map<std::string, std::vector<std::vector<std::string>>>& data,
+				 const std::map<std::string, std::map<std::string, int>>& data_info,
+				 std::map<std::string, std::map<std::string, int>>& confusion_matrix) {
+
+	int total(data_info.at(nbdc::total_cat).at("Docs"));
+	int correct(0);	
+	
+	for(auto const& map : data) {
+		for(auto const& vector : map.second) {
+			std::vector<std::pair<std::string, double>> prob_class;
+			for(auto const& query: processed_data) {
+				double line_prob(0.0);
+				for(auto const& word : vector) {
+					double temp(0.0);
+					try {
+						temp = query.second.at(word);
+						line_prob += std::log(temp);
+					}catch(const std::out_of_range) {
+
+					}
+				}
+				double temp(processed_data_info.at(query.first).at("Docs"));
+				line_prob += std::log(temp);
+				std::pair<std::string, double> p(query.first, line_prob);
+				prob_class.push_back(p);
+			}
+			auto highest_prob = std::max_element(begin(prob_class), end(prob_class),
+            [](const std::pair<std::string, double>& lhs,
+            	const std::pair<std::string, double>& rhs){
+            return lhs.second < rhs.second;});
+
+            //std::cout << "(" << (*highest_prob).first << ","<< map.first << ")" << std::endl;
+            confusion_matrix[map.first][(*highest_prob).first]++;
+
+            if((*highest_prob).first == map.first) {
+            	correct++;
+            }
+
+		}
+	}
+
+	std::cout << "Total              : " << total << std::endl;
+	std::cout << "Correct            : " << correct << std::endl;
+	std::cout << "Accuracy           : " << (double)correct / (double)total * 100 
+				<< "%" << std::endl;
+};
+
+void PrintConfusionMatrix(const std::map<std::string, std::map<std::string, int>>& confusion_matrix) {
+	std::ostringstream rows;
+	std::ostringstream head;
+	int count(0);
+	int width(17);
+	head << "| ";
+	nbdc::PrintElement<std::string>("", head, width);
+
+	for(auto const& matrix : confusion_matrix) {
+		width = 17;
+		head << "| ";
+		nbdc::PrintElement<std::string>(matrix.first, head, matrix.first.size() + 1);
+		rows << "| ";
+		//head << "| ";
+		nbdc::PrintElement<std::string>(matrix.first, rows, width);
+		for(auto const& el : matrix.second){
+			width = el.first.size() + 1;
+			rows << "| ";
+			nbdc::PrintElement<int>(el.second, rows, width);
+		}
+		rows << "|" << std::endl;
+	}
+	head << "| ";
+	std::cout << std::left<< std::setw(head.str().size() - 1) << std::setfill('-') << "" << std::endl;;
+	std::cout << head.str() << std::endl;
+	std::cout << std::left<< std::setw(head.str().size() -1 ) << std::setfill('-') << "" << std::endl;;
+	std::cout << rows.str() << std::endl;
 };
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -5,229 +5,18 @@
 #include <map>
 #include <chrono>
 #include <complex>
+#include <algorithm>
+#include <iomanip>
 
 #include <omp.h>
 
 #include "nbdc_config.hpp"
 #include "Func/func.hpp"
 
-
-std::string total_category_word_count("lyj7MepVPo8m8N8o");
-std::string total_cat("F1vs8KymEXNmmIod");
-int total_docs(0);
-
-
 void version(const std::string & cmd) {
     fprintf(stderr,"%s Version %d.%d.%d\n", cmd.c_str(), nbdc_VERSION_MAJOR,
   			nbdc_VERSION_MINOR, nbdc_VERSION_PATCH);
 }
-
-void ReadTrFile(std::string filename,
-			  std::map<std::string, std::ostringstream>& data,
-			  std::map<std::string, std::map<std::string, int>> & data_info) {
-	//Opening the file
-	std::ifstream in_file(filename);
-	//Where the lines from the file will be stored
-	std::string line;
-
-	//While loop to get the line from the file
-	while(std::getline(in_file, line)) {
-		//A line from the file
-		std::istringstream in_line(line);
-		//Buffer to build up string. Using it locally, saves ~1s
-		std::ostringstream os;
-		//Where the word in the line will be stored
-		std::string token;
-		//Counter varaible for pos
-		int count(0);
-		//File seperator
-		char sep = ' ';
-		//Category string
-		std::string cat("");
-
-		//Loop to get each "word" out of the line
-		while(std::getline(in_line, token, sep)) {
-			//The first "word" is the category, that I can't use and is only for
-			//training sets
-			if(count == 0) {
-				cat += token;
-				count++;
-				data_info[cat]["Docs"]++;
-				total_docs++;
-				continue;
-			}
-			//Eliminates unwanted spaces in my stream
-			if(count > 1) {
-				os << " ";
-			}
-
-			//Adds the token to the local stream
-			os << token;
-			count++;
-		}
-		data_info[cat]["Words"] += (count - 1);
-		//Add the stream to the passed in stream
-		data[cat] << " " << os.str();
-	}
-	int temp_docs(0);
-	int temp_words(0);
-	for(auto const& map : data_info) {
-		temp_docs += map.second.at("Docs");
-		temp_words += map.second.at("Words");
-	}
-
-	data_info[total_cat]["Docs"] = temp_docs;
-	data_info[total_cat]["Words"] = temp_words;
-}
-
-void ProcessData(const std::map<std::string, std::ostringstream>& data,
-				 const std::map<std::string, std::map<std::string, int>> & data_info,
-				 std::map<std::string, std::map<std::string, double>>& processed_data,
-				 std::map<std::string, std::map<std::string, double>>& processed_data_info,
-				 std::map<std::string, std::map<std::string, int>>& cat_vocab,
-				 std::map<std::string, int>& vocab) {
-
-	//Gets the indivudal category vocab
-	for(auto const& map : data) {
-		std::istringstream iss(map.second.str());
-		std::string word;
-		while(iss >> word) {
-			cat_vocab[map.first][word]++;
-		}
-		for(auto const& word : cat_vocab[map.first]) {
-			cat_vocab[map.first][total_category_word_count] += word.second;
-		}
-	}
-
-	//Gets the total counts of all words found
-	for(auto const& map_o : cat_vocab) {
-		for(auto const& map_i : map_o.second) {
-			vocab[map_i.first] += map_i.second;
-		}
-	}
-
-	int total_words = vocab.size();
-	int total_docs = data_info.at(total_cat).at("Docs");
-
-	//Calculaltes the probability of the word for each category
-	for(auto const& map_o : cat_vocab) {
-		//map_o.first is the category string
-		//map_o.second is the map of word counts
-		double cat_total_words = 
-					cat_vocab[map_o.first][total_category_word_count];
-		int cat_total_docs = data_info.at(map_o.first).at("Docs");
-		processed_data_info[map_o.first]["Doc_p"] = (double)cat_total_docs / (double)total_docs;
-		//std::cout << cat_total_docs << "/" << total_docs << " = "<< cat_doc_count[map_o.first] << std::endl;
-
-		for(auto const& map_i : map_o.second) {
-			//map_i.first is the word string
-			//map_i.second is the word count for the category
-			// std::cout << "(" << map_i.second << "+" << 1;
-			// std::cout << ") / (" << cat_total_words << "+" << total_words << ")"
-			// 			<< std::endl;
-			processed_data[map_o.first][map_i.first] =
-						((double)map_i.second + (double)1) /
-						(cat_total_words + (double)total_words);
-		}
-	}
-}
-
-void ReadTeFile(std::string filename,
-			  std::map<std::string, std::vector<std::string>>& data) {
-	//Opening the file
-	std::ifstream in_file(filename);
-	//Where the lines from the file will be stored
-	std::string line;
-
-	//While loop to get the line from the file
-	while(std::getline(in_file, line)) {
-		//A line from the file
-		std::istringstream in_line(line);
-		//Buffer to build up string. Using it locally, saves ~1s
-		std::ostringstream os;
-		//Where the word in the line will be stored
-		std::string token;
-		//Counter varaible for pos
-		int count(0);
-		//File seperator
-		char sep = ' ';
-		//Category string
-		std::string cat("");
-
-		//Loop to get each "word" out of the line
-		while(std::getline(in_line, token, sep)) {
-			//The first "word" is the category, that I can't use and is only for
-			//training sets
-			if(count == 0) {
-				cat += token;
-				count++;
-				continue;
-			}
-			//Eliminates unwanted spaces in my stream
-			if(count > 1) {
-				os << " ";
-			}
-
-			//Adds the token to the local stream
-			os << token;
-			count++;
-		}
-		//Add the stream to the passed in stream
-		data[cat].push_back(os.str());
-	}
-}
-
-void AnalyzeData(const std::map<std::string, std::map<std::string, double>>& processed_data,
-				 const std::map<std::string, std::map<std::string, double>>& processed_data_info,
-				 const std::map<std::string, std::map<std::string, int>>& data_info,
-				 const std::map<std::string, std::vector<std::string>>& data) {
-
-	int total(0);
-	int correct(0);
-	for(auto const& map : data) {
-		for(auto const& line : map.second) {
-			std::map<std::string, double> probability_classification;
-			for(auto const& map_d : processed_data){
-				std::istringstream iss(line);
-				std::string word;
-				int count(0);
-				double w_p(0.0);
-
-				while(iss >> word) {
-					if(count == 0) {
-						count++;
-						continue;
-					}
-					if(!(map_d.second.find(word) == map_d.second.end())) {
-						w_p += std::log(map_d.second.at(word));
-					}
-				}
-				double c_p(processed_data_info.at(map_d.first).at("Doc_p"));
-				//std::cout << c_p << std::endl;
-				probability_classification[map_d.first] = std::log(c_p) + w_p;
-			}
-
-			std::pair<std::string, double> result("", 10000000.0);
-			for(auto const& map_r : probability_classification) {
-				//std::cout << result.second << " < " << map_r.second << std::endl;
-				if(result.second > map_r.second) {
-					result.first = map_r.first;
-					result.second = map_r.second;
-				}
-			}
-			if(result.first == map.first) {
-				correct++;
-			}
-			total++;
-		}
-	}
-
-	std::cout << "Total              : " << total << std::endl;
-	std::cout << "Correct            : " << correct << std::endl;
-	std::cout << "Accuracy           : " << (double)correct / (double)total 
-				<< "%" << std::endl;
-}
-
 
 int main(int argc, char * argv[]){
 
@@ -254,8 +43,11 @@ int main(int argc, char * argv[]){
 		exit(EXIT_FAILURE);
 	}
 
-	std::map<std::string, std::ostringstream> tr_data;
-	std::map<std::string, std::map<std::string, int>> data_info;
+	//Base training data
+	std::map<std::string, std::vector<std::vector<std::string>>> tr_data;
+	std::map<std::string, std::map<std::string, int>> tr_data_info;
+
+	//Processed data
 	std::map<std::string, int> vocab;
 	std::map<std::string, std::map<std::string, int>> cat_vocab;
 	std::map<std::string, std::map<std::string, double>> processed_data;
@@ -265,7 +57,7 @@ int main(int argc, char * argv[]){
 	 *	Read the training data in to a data type to easily process
 	 */
 	auto start = std::chrono::high_resolution_clock::now();
-	ReadTrFile(tr_filename, tr_data, data_info);
+	nbdc::ReadFile(tr_filename, tr_data, tr_data_info);
 	auto end = std::chrono::high_resolution_clock::now();
 	auto time_read1 = end - start;
 	std::cout << "Training Read Time : " <<
@@ -276,7 +68,7 @@ int main(int argc, char * argv[]){
 	 *	Process data out into a data type to easily manage
 	 */
 	start = std::chrono::high_resolution_clock::now();
-	ProcessData(tr_data, data_info, processed_data, processed_data_info, cat_vocab, vocab);
+	nbdc::ProcessData(tr_data, tr_data_info, processed_data, processed_data_info, cat_vocab, vocab);
 	end = std::chrono::high_resolution_clock::now();
 	auto time_process = end - start;
 	std::cout << "Process Time       : " <<
@@ -286,9 +78,11 @@ int main(int argc, char * argv[]){
 	/**
 	 *	Read the Test data in to a data type to easily process
 	 */
-	std::map<std::string, std::vector<std::string>> te_data;
+	//Base test data
+	std::map<std::string, std::vector<std::vector<std::string>>> te_data;
+	std::map<std::string, std::map<std::string, int>> te_data_info;
 	start = std::chrono::high_resolution_clock::now();
-	ReadTeFile(te_filename, te_data);
+	nbdc::ReadFile(te_filename, te_data, te_data_info);
 	end = std::chrono::high_resolution_clock::now();
 	auto time_read2 = end - start;
 	std::cout << "Test Read Time     : " <<
@@ -298,17 +92,29 @@ int main(int argc, char * argv[]){
 	/**
 	 *	Analyze the data based on the training data.
 	 */
+	std::map<std::string, std::map<std::string, int>> confusion_matrix;
 	start = std::chrono::high_resolution_clock::now();
-	AnalyzeData(processed_data, processed_data_info, data_info, te_data);
+	nbdc::AnalyzeData(processed_data, processed_data_info, te_data, te_data_info, confusion_matrix);
 	end = std::chrono::high_resolution_clock::now();
 	auto time_analyze = end - start;
 	std::cout << "Analyze Time       : " <<
 				(std::chrono::duration_cast<std::chrono::nanoseconds>
 				(time_analyze).count()) / 1000000000.0 << "s" << std::endl;
 
+	for(auto const& matrix1 : confusion_matrix) {
+		for(auto const& matrix2 : confusion_matrix) {
+			if(matrix1.second.find(matrix2.first) == matrix1.second.end()) {
+				confusion_matrix[matrix1.first][matrix2.first] = 0;
+			}
+		}
+
+	}
+
 	auto total_run_time = time_analyze + time_read2 + time_process + time_read1;
 	std::cout << "Total Run Time     : " <<
 				(std::chrono::duration_cast<std::chrono::nanoseconds>
 				(total_run_time).count()) / 1000000000.0 << "s" << std::endl;
+
+	nbdc::PrintConfusionMatrix(confusion_matrix);
 	exit(EXIT_SUCCESS);
 }
